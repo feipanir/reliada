cda8548d3c50a78d61140527f863a25c6921e43a
quan5609 <jhoh94@naver.com>
2023-10-17 20:53:55 +0900

    first commit

---
+++ dmadapt/seg/run_experiments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# rm -rf ./configs/generated/* ./work_dirs/* 
+
+
+# run advseg, minent, dacs, hrda, daformer
+# python run_experiments.py --exp 81
+
+
+# run advseg, minent, dacs, hrda, daformer without masking 
+python run_experiments.py --exp 815
